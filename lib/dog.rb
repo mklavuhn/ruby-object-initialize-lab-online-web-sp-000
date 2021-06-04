2b3377a780e130579fcffcd9cc8c02889a24bccc
Michael C Klavuhn <michaelklavuhn3@gmail.com>
2021-06-04 17:35:02 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,9 +1,27 @@
 class Dog 
   
-  def initialize(name)
+  def initialize(name, breed)
+    @name = name 
+    @breed = breed 
+    if breed == nil 
+      puts "Mutt"
+  end
+  
+  def name 
     @name = name 
   end
   
+  def name=(new_name)
+    @name = new_name
+  end
+  
+  def breed 
+    @breed = breed 
+  end
+  
+  def breed=(initial_breed)
+    @breed = initial_breed
+  end
   
 end
     
